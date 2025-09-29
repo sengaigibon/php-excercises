68fdee3aa4a759fdffd70578235e13edb0aaa89f
Javier Knight <jrcaballerob@gmail.com>
2025-09-29 11:38:55 +0200

    Add: PHPUnit config and sample test

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class SimpleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
