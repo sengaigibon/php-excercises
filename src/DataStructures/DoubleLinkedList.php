c5bafa21ad0315979157597f20e249edfa2b80ab
Javier Knight <jrcaballerob@gmail.com>
2025-10-01 16:12:57 +0200

    Update linked list implementation and tests

--- src/DataStructures/DoubleLinkedList.php
+++ src/DataStructures/DoubleLinkedList.php
@@ -24,7 +24,17 @@ class DoubleLinkedList
 
     public function addAtEnd(string $value): void
     {
-        // TBD
+        $newNode = new ListNode($value);
+
+        if (!$this->tail) {
+            $this->head = $newNode;
+            $this->tail = $newNode;
+            return;
+        }
+
+        $this->tail->next = $newNode;
+        $newNode->previous = $this->tail;
+        $this->tail = $newNode;
     }
 
     public function traverse(string $direction = 'forward'): string
@@ -91,5 +101,56 @@ class DoubleLinkedList
         }
 
         unset($currentNode);
+    }
+
+    public function removeAny(string $value): void
+    {
+        $node = $this->find($value);
+
+        if ($node === null) {
+            return;
+        }
+
+        $previous = $node->previous;
+        $next = $node->next;
+
+        if ($previous === $next) {
+            $this->head = $this->tail = null;
+            unset($node, $previous, $next);
+            return;
+        }
+
+        if ($previous) {
+            $previous->next = $next;
+        }
+
+        if ($next) {
+            $next->previous = $previous;
+        }
+
+        if ($this->head === $node) {
+            $this->head = $next;
+        }
+
+        if ($this->tail === $node) {
+            $this->tail = $previous;
+        }
+
+        unset($node);
+    }
+
+    private function find(string $value): ?ListNode
+    {
+        $current = $this->head;
+
+        while ($current) {
+            if ($current->value === $value) {
+                return $current;
+            }
+
+            $current = $current->next;
+        }
+
+        return null;
     }
 }
--- tests/Excercises/DataStructures/LinkedLists.php
+++ tests/Excercises/DataStructures/LinkedLists.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class LinkedLists extends TestCase
 {
-    public function testListFunctions(): void
+    public function testAddAtStart(): void
     {
         $list = new DoubleLinkedList();
 
@@ -20,6 +20,18 @@ class LinkedLists extends TestCase
 
         $this->assertEquals('a6 -> a5 -> a4 -> a3 -> a2 -> a1', $list->traverse());
         $this->assertEquals('a1 -> a2 -> a3 -> a4 -> a5 -> a6', $list->traverse('backward'));
+    }
+
+    public function testRemove(): void
+    {
+        $list = new DoubleLinkedList();
+
+        $list->addAtStart('a1');
+        $list->addAtStart('a2');
+        $list->addAtStart('a3');
+        $list->addAtStart('a4');
+        $list->addAtStart('a5');
+        $list->addAtStart('a6');
 
         $list->removeFirst();
         $this->assertEquals('a5 -> a4 -> a3 -> a2 -> a1', $list->traverse());
@@ -28,5 +40,85 @@ class LinkedLists extends TestCase
         $this->assertEquals('a5 -> a4 -> a3 -> a2', $list->traverse());
     }
 
+    public function testAddAtEnd(): void
+    {
+        $list = new DoubleLinkedList();
+
+        $list->addAtEnd('a1');
+        $list->addAtEnd('a2');
+        $list->addAtEnd('a3');
+        $list->addAtEnd('a4');
+        $list->addAtEnd('a5');
+        $list->addAtEnd('a6');
+
+        $this->assertEquals('a1 -> a2 -> a3 -> a4 -> a5 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a5 -> a4 -> a3 -> a2 -> a1', $list->traverse('backward'));
+    }
+
+    public function testRemoveAny(): void
+    {
+        $list = new DoubleLinkedList();
+
+        $list->addAtEnd('a1');
+        $list->addAtEnd('a2');
+        $list->addAtEnd('a3');
+        $list->addAtEnd('a4');
+        $list->addAtEnd('a5');
+        $list->addAtEnd('a6');
+
+        $this->assertEquals('a1 -> a2 -> a3 -> a4 -> a5 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a5 -> a4 -> a3 -> a2 -> a1', $list->traverse('backward'));
+
+        $list->removeAny('a5');
+
+        $this->assertEquals('a1 -> a2 -> a3 -> a4 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a4 -> a3 -> a2 -> a1', $list->traverse('backward'));
+
+        $list->removeAny('a1');
+        $this->assertEquals('a2 -> a3 -> a4 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a4 -> a3 -> a2', $list->traverse('backward'));
+
+        $list->removeAny('a3');
+        $this->assertEquals('a2 -> a4 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a4 -> a2', $list->traverse('backward'));
+
+        $list->removeAny('a4');
+        $this->assertEquals('a2 -> a6', $list->traverse());
+        $this->assertEquals('a6 -> a2', $list->traverse('backward'));
+
+        $list->removeAny('a6');
+        $this->assertEquals('a2', $list->traverse());
+        $this->assertEquals('a2', $list->traverse('backward'));
+
+        $list->removeAny('a2');
+        $this->assertEquals('', $list->traverse());
+        $this->assertEquals('', $list->traverse('backward'));
+    }
+
+    public function testRemoveLastWhenTheresOnlyOneNode(): void
+    {
+        $list = new DoubleLinkedList();
+        $list->addAtEnd('a1');
+
+        $this->assertEquals('a1', $list->traverse());
+        $this->assertEquals('a1', $list->traverse('backward'));
+
+        $list->removeLast();
+        $this->assertEquals('', $list->traverse());
+        $this->assertEquals('', $list->traverse('backward'));
+    }
+
+    public function testRemoveFirstWhenTheresOnlyOneNode(): void
+    {
+        $list = new DoubleLinkedList();
+        $list->addAtEnd('a1');
+
+        $this->assertEquals('a1', $list->traverse());
+        $this->assertEquals('a1', $list->traverse('backward'));
+
+        $list->removeFirst();
+        $this->assertEquals('', $list->traverse());
+        $this->assertEquals('', $list->traverse('backward'));
+    }
 }
 
